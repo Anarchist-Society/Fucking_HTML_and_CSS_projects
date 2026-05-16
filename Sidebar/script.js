11ab107283347d7ffe8cdcf1286950627e1dc456
Anarchist-Society <220625772+Anarchist-Society@users.noreply.github.com>
2026-05-16 13:58:09 +0200

    update sidebar script.js

--- Sidebar/script.js
+++ Sidebar/script.js
@@ -1,1 +1,4 @@
-const toggleSidebar = () => document.body.classList.toggle("open");
+const toggleSidebar = () => {
+  const isOpen = document.body.classList.toggle("open");
+  document.querySelector(".sidebar-burger").setAttribute("aria-expanded", isOpen);
+};
